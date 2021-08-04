7e6edf971f5c875c747974d16b6713029eecb59e
Oscar <osccamacho@gmail.com>
2021-08-04 18:46:14 -0500

    se realizo promedio ponderado

--- ponderado.js
+++ ponderado.js
@@ -1,10 +1,40 @@
-const notas = {
-    5: '0.2',
-    3: '0.3',
-    1: '0.1',
-    4: '0.2',
-    2: '0.3',
-};
+const notas = [
+    {curso: "Matematicas",
+    nota: 3,
+    creditos: 5},
+    {curso: "Fisica",
+    nota: 3,
+    creditos: 3},
+    {curso: "Espanol",
+    nota: 5,
+    creditos: 2},
+    {curso: "Ingles",
+    nota: 4,
+    creditos: 3},
+];
 
 
-const ponderado = for ()
+const notesWithCredit = notas.map(function (noteObject) {
+    return noteObject.nota * noteObject.creditos;
+});
+
+const sumOfNotesWithCredit = notesWithCredit.reduce(
+    function (sum = 0, newVal) {
+        return sum + newVal;
+    }
+);
+
+const credits = notas.map(function (noteObject) {
+    return noteObject.creditos;
+});
+
+const sumOfCredits = credits.reduce(
+    function (sum = 0, newVal) {
+        return sum + newVal;
+    }
+);
+
+
+const resultado = sumOfNotesWithCredit / sumOfCredits
+
+console.log(resultado);
